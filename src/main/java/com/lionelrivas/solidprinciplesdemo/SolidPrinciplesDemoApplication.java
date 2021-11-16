71b8489aebe15a4b60dc442fdff48f3d52e097ad
lionelrrivas <lionel.r.rivas@gmail.com>
2021-11-16 17:01:42 -0600

    initial commit

---
+++ src/main/java/com/lionelrivas/solidprinciplesdemo/SolidPrinciplesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lionelrivas.solidprinciplesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidPrinciplesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidPrinciplesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lionelrivas/solidprinciplesdemo/SolidPrinciplesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionelrivas.solidprinciplesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidPrinciplesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
